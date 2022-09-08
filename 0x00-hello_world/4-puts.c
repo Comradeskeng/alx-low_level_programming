805e4efe468ad1487fcb2992c0d1556c0eba5e82
Comradeskeng <olonahabeeb123@gmail.com>
2022-09-08 14:11:54 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,6 @@
  * * main: the puts(Programming is like building a multilingual puzzle)
  * * return: Always 0 (success)
  * **/
-
 int main(void)
-{
-
-   puts("\"Programming is like building a multilingual puzzle");
-
-   return (0);
-
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}
