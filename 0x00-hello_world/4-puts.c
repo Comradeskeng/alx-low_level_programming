8f126c86a76d6a3e322492f0bbc2be48956b7a98
Comradeskeng <olonahabeeb123@gmail.com>
2022-09-08 10:47:35 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * * main - Starting point
+ * *
+ * * main: the puts(Programming is like building a multilingual puzzle)
+ * * return: Always 0 (success)
+ * **/
+
+int main(void)
+{
+
+   puts("\"Programming is like building a multilingual puzzle");
+
+   return (0);
+
+}
